450f95f2151aaa3c9483c782c193c0e12d6af8fe
Mono <125167202+Mono0087@users.noreply.github.com>
2023-08-05 00:28:22 +0500

    Finish project

--- main.js
+++ main.js
@@ -13,6 +13,13 @@ function Book(name, author, pages, readStatus) {
         this.pages = pages,
         this.readStatus = readStatus
 }
+Book.prototype.changeReadStatus = function () {
+    if (this.readStatus) {
+        this.readStatus = false;
+    } else {
+        this.readStatus = true;
+    }
+}
 
 function reloadBooksContainer() {
     booksContainer.innerHTML = ''
@@ -20,18 +27,37 @@ function reloadBooksContainer() {
 
 function appendBooksContainer(books) {
     reloadBooksContainer()
-    let count = 0;
+    let bookIdCount = 0;
     books.forEach(book => {
         let cardExample = document.querySelector('.card');
         let cardEl = cardExample.cloneNode(true);
-        cardEl.dataset.bookId = count;
+        cardEl.dataset.bookId = bookIdCount;
         cardEl.children[0].textContent = book.name;
         cardEl.children[1].textContent = book.author;
         cardEl.children[2].children[0].textContent = book.pages
         cardEl.children[3].children[1].checked = book.readStatus;
         booksContainer.prepend(cardEl)
-        count++;
+        bookIdCount++;
     });
+}
+
+function clearFormInputs() {
+    for (let i = 0; i < 3; ++i) {
+        inputs[i].value = '';
+    }
+    inputs[3].checked = false;
+}
+
+function addBookToLibrary(name, author, pages, readStatus) {
+    if (name !== '' && author !== '' && pages !== '') {
+        let book = new Book(name, author, pages, readStatus);
+        bookShelf.push(book);
+        clearFormInputs();
+        hideForm();
+        appendBooksContainer(bookShelf);
+    } else {
+        alert('Please enter all fields!')
+    }
 }
 
 addBtn.addEventListener('click', (e) => {
@@ -53,51 +79,24 @@ function hideForm() {
     });
 }
 
-document.body.addEventListener('click', (Event) => {
-    if (Event.target.classList.contains('card')){
-        console.log(Event.target.dataset.bookId)
-    }
-    console.log(bookShelf)
-})
-
 booksContainer.addEventListener('click', (Event) => {
-    if (Event.target.classList.contains('remove_btn')) {
-        bookShelf.splice(+Event.target.parentElement.dataset.bookId, 1);
-        Event.target.parentElement.remove();
-        appendBooksContainer(bookShelf)
-}
-})
-
-function clearFormInputs() {
-    for (let i = 0; i < 3; ++i) {
-        inputs[i].value = '';
+    let cardInteraction = Event.target.classList[0];
+    switch (cardInteraction) {
+        case 'remove_btn':
+            bookShelf.splice(+Event.target.parentElement.dataset.bookId, 1);
+            Event.target.parentElement.remove();
+            appendBooksContainer(bookShelf)
+            break;
+        case 'status_checkbox':
+            bookShelf[Event.target.parentElement.parentElement.dataset.bookId].changeReadStatus()
+            break;
     }
-    inputs[3].checked = false;
-}
+})
 
 popUpForm.addEventListener('click', (e) => {
-    if (e.target.tagName === 'BUTTON') {
-        let newTitle = inputs[0].value;
-        let newAuthor = inputs[1].value;
-        let newPagesNum = inputs[2].value;
-        let newReadStatus = inputs[3].checked;
-        if (newTitle !== '' && newAuthor !== '' && newPagesNum !== '') {
-            let book = new Book(newTitle, newAuthor, newPagesNum, newReadStatus);
-            bookShelf.push(book);
-            clearFormInputs();
-            hideForm();
-            appendBooksContainer(bookShelf);
-        } else {
-            alert('Please enter all fields!')
-        }
+    if (e.target.classList.contains('add_new-book_btn')) {
+        addBookToLibrary(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].checked);
         e.preventDefault();
     }
     e.stopPropagation()
 })
-
-
-
-
-
-
-console.log(addBtn.style.color)
