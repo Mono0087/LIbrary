746e6fecac22a494a60a1640993677dc3eb25475
Mono <125167202+Mono0087@users.noreply.github.com>
2023-08-24 09:54:58 +0500

    Refactor code using module pattern

--- main.js
+++ main.js
@@ -1,102 +1,97 @@
-const addBtn = document.querySelector('.add_btn');
-const addBookBtn = document.querySelector('.add_new-book_btn')
-const booksContainer = document.querySelector('.bookcards_container');
-const overlay = document.querySelector('#overlay');
-const popUpForm = document.querySelector('#pop-up-form');
-let inputs = popUpForm.getElementsByTagName('input');
+const Library = (function () {
+    const books = [];
 
-let bookShelf = [];
-
-function Book(name, author, pages, readStatus) {
-    this.name = name,
-        this.author = author,
-        this.pages = pages,
-        this.readStatus = readStatus
-}
-Book.prototype.changeReadStatus = function () {
-    if (this.readStatus) {
-        this.readStatus = false;
-    } else {
-        this.readStatus = true;
+    // New objects creation
+    const _bookFactory = function (name, author, pages, read) {
+        function changeReadStatus() {
+            if (this.read) {
+                this.read = false;
+            } else this.read = true;
+        }
+        return { name, author, pages, read, changeReadStatus }
     }
-}
 
-function reloadBooksContainer() {
-    booksContainer.innerHTML = ''
-}
+    // Cache DOM
+    const libraryDOM = document.querySelector('#library_module');
+    const addBtn = libraryDOM.querySelector('.add_btn');
+    const booksContainer = libraryDOM.querySelector('.bookcards_container');
+    const overlay = libraryDOM.querySelector('#overlay');
+    const popUpForm = libraryDOM.querySelector('#pop-up-form');
+    const addNewBookBtn = popUpForm.querySelector('#add_new-book_btn');
+    const bookCardTemplate = libraryDOM.querySelector('.card-template').children[0];
 
-function appendBooksContainer(books) {
-    reloadBooksContainer()
-    let bookIdCount = 0;
-    books.forEach(book => {
-        let cardExample = document.querySelector('.card');
-        let cardEl = cardExample.cloneNode(true);
-        cardEl.dataset.bookId = bookIdCount;
-        cardEl.children[0].textContent = book.name;
-        cardEl.children[1].textContent = book.author;
-        cardEl.children[2].children[0].textContent = book.pages
-        cardEl.children[3].children[1].checked = book.readStatus;
-        booksContainer.prepend(cardEl)
-        bookIdCount++;
-    });
-}
+    _render();
+    // Bind Events
+    addBtn.addEventListener('click', _showPopUpForm);
+    overlay.addEventListener('click', _hidePopUpForm);
+    popUpForm.addEventListener('click', _handleForm);
+    booksContainer.addEventListener('click', _handleContainer);
 
-function clearFormInputs() {
-    for (let i = 0; i < 3; ++i) {
-        inputs[i].value = '';
+    // Private methods
+    function _render() {
+        booksContainer.innerHTML = '';
+        books.forEach(book => {
+            let cardEl = bookCardTemplate.cloneNode(true);
+            cardEl.children[0].textContent = book.name;
+            cardEl.children[1].textContent = book.author;
+            cardEl.children[2].children[0].textContent = book.pages
+            cardEl.children[3].children[1].checked = book.read;
+            booksContainer.append(cardEl);
+        });
     }
-    inputs[3].checked = false;
-}
-
-function addBookToLibrary(name, author, pages, readStatus) {
-    if (name !== '' && author !== '' && pages !== '') {
-        let book = new Book(name, author, pages, readStatus);
-        bookShelf.push(book);
-        clearFormInputs();
-        hideForm();
-        appendBooksContainer(bookShelf);
-    } else {
-        alert('Please enter all fields!')
+    function _showPopUpForm() {
+        overlay.classList.remove('hidden');
+        setTimeout((function () {
+            overlay.classList.remove('visually-hidden');
+        }), 5)
     }
-}
-
-addBtn.addEventListener('click', (e) => {
-    overlay.classList.remove('hidden');
-    setTimeout(function () {
-        overlay.classList.remove('visually-hidden')
-    }, 5)
-})
-
-overlay.addEventListener('click', hideForm)
-function hideForm() {
-    overlay.classList.add('visually-hidden');
-    overlay.addEventListener('transitionend', function (e) {
-        overlay.classList.add('hidden');
-    }, {
-        capture: false,
-        once: true,
-        passive: false
-    });
-}
-
-booksContainer.addEventListener('click', (Event) => {
-    let cardInteraction = Event.target.classList[0];
-    switch (cardInteraction) {
-        case 'remove_btn':
-            bookShelf.splice(+Event.target.parentElement.dataset.bookId, 1);
-            Event.target.parentElement.remove();
-            appendBooksContainer(bookShelf)
-            break;
-        case 'status_checkbox':
-            bookShelf[Event.target.parentElement.parentElement.dataset.bookId].changeReadStatus()
-            break;
+    function _hidePopUpForm() {
+        overlay.classList.add('visually-hidden');
+        overlay.addEventListener('transitionend', function (e) {
+            overlay.classList.add('hidden');
+        }, {
+            capture: false,
+            once: true,
+            passive: false
+        });
     }
-})
-
-popUpForm.addEventListener('click', (e) => {
-    if (e.target.classList.contains('add_new-book_btn')) {
-        addBookToLibrary(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].checked);
-        e.preventDefault();
+    function _handleForm(e) {
+        if (e.target === addNewBookBtn) {
+            let inputsData = [...popUpForm.querySelectorAll('input')].map((el) => {
+                if (el.type === 'checkbox') return el.checked;
+                return el.value;
+            })
+            addBook(...inputsData);
+            _hidePopUpForm();
+            e.preventDefault();
+        }
+        e.stopPropagation();
     }
-    e.stopPropagation()
-})
+    function _handleContainer(e) {
+        let el = e.target.closest('.card');
+        let index = Array.from(booksContainer.children).indexOf(el);
+        let cardInteraction = e.target.classList[0];
+        switch (cardInteraction) {
+            case 'remove_btn':
+                deleteBook(index);
+                break;
+            case 'status_checkbox':
+                books[index].changeReadStatus();
+                break;
+        }
+    }
+
+    // Public methods
+    function addBook(name, author, pages, status) {
+        if (arguments.length < 4) throw Error('Wrong input!')
+        books.push(_bookFactory(...arguments))
+        _render();
+        return books;
+    }
+    function deleteBook(index) {
+        books.splice(index, 1);
+        _render();
+        return books;
+    }
+    return { addBook, deleteBook}
+})()
